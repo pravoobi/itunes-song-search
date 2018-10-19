c839239523943f6b197faaa5af617c931ca125ad
Venkata Praveen Kumar Velisetty <pravoobi@gmail.com>
2018-10-20 02:15:19 +0530

    songs list fetched from api and displayed

--- script.js
+++ script.js
@@ -7,10 +7,47 @@ let textInput = document.querySelector('#textInput');
 	}
 
   function millisToMinutesAndSeconds(millis) {
-  var minutes = Math.floor(millis / 60000);
-  var seconds = ((millis % 60000) / 1000).toFixed(0);
-  return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-}
+    var minutes = Math.floor(millis / 60000);
+    var seconds = ((millis % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+  }
+  function getSongDetails(trackId){
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://itunes.apple.com/lookup?id='+ trackId);
+  
+        xhr.onload = function(){
+          if(this.status == 200){
+            let shtml = '';
+            let parsed = JSON.parse(this.responseText);
+            for(var i=0; i < parsed.resultCount ; i++){
+            shtml += '<div class="SongItem" onclick="songDetails('+parsed.results[i].trackId +')"><div class="LeftItem"><h2>' + parsed.results[i].trackName + 
+            '<h2><div class="AlbumArtist"><p>' + parsed.results[i].collectionName+'</p><p> &#8226; </p> <p>' + parsed.results[i].artistName+ 
+            '</p> </div></div><div class="RightItem"> <p>' + millisToMinutesAndSeconds(parsed.results[i].trackTimeMillis)+'</p><p><i class="far fa-heart"></i></p></div></div>';
+            console.log("parsed", parsed.results[i]);
+            }
+            results.innerHTML = shtml;
+          }
+        }
+  
+        xhr.send(); 
+  }
+
+  var modal = document.getElementById('songModal');
+  
+  function songDetails(trackId){
+      modal.style.display = "block";
+      getSongDetails(trackId);
+  }
+
+  var span = document.getElementsByClassName("close")[0];
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+  }
 
     // Fetch with XHR
     function getItunesAjax(){
@@ -22,12 +59,14 @@ let textInput = document.querySelector('#textInput');
   
         xhr.onload = function(){
           if(this.status == 200){
-            // fact.style.display = 'block';
+            let shtml = '';
             let parsed = JSON.parse(this.responseText);
-            let shtml = '<div class="SongItem"><div class="LeftItem"><h2>' + parsed.results[0].trackName + 
-            '<h2><div class="AlbumArtist"><p>' + parsed.results[0].collectionName+'</p><p> &#8226; </p> <p>' + parsed.results[0].artistName+ 
-            '</p> </div></div><div class="RightItem"> <p>' + millisToMinutesAndSeconds(parsed.results[0].trackTimeMillis)+'</p><p><i class="fas fa-heart"></i></p>';
-            console.log("parsed", parsed.results[0]);
+            for(var i=0; i < parsed.resultCount ; i++){
+            shtml += '<div class="SongItem" onclick="songDetails('+parsed.results[i].trackId +')"><div class="LeftItem"><h2>' + parsed.results[i].trackName + 
+            '<h2><div class="AlbumArtist"><p>' + parsed.results[i].collectionName+'</p><p> &#8226; </p> <p>' + parsed.results[i].artistName+ 
+            '</p> </div></div><div class="RightItem"> <p>' + millisToMinutesAndSeconds(parsed.results[i].trackTimeMillis)+'</p><p><i class="far fa-heart"></i></p></div></div>';
+            console.log("parsed", parsed.results[i]);
+            }
             results.innerHTML = shtml;
           }
         }
