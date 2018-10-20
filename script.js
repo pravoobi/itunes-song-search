90164242d03cac072a1a4e64377ebc1bf019e5fb
Venkata Praveen Kumar Velisetty <pravoobi@gmail.com>
2018-10-20 11:23:19 +0530

    songs details fetch and displayed dynamically

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let results = document.querySelector('#results');
-
-let textInput = document.querySelector('#textInput');
+var results = document.querySelector('#results');
+var modal = document.getElementById('songModal');
+var songcontent = document.getElementById('songcontent');
+var textInput = document.querySelector('#textInput');
     console.log(textInput);
 	if(textInput){
     textInput.addEventListener('input', getItunesAjax);
@@ -11,28 +12,27 @@ let textInput = document.querySelector('#textInput');
     var seconds = ((millis % 60000) / 1000).toFixed(0);
     return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
   }
+
   function getSongDetails(trackId){
-        let xhr = new XMLHttpRequest();
+        var xhr = new XMLHttpRequest();
         xhr.open('GET', 'https://itunes.apple.com/lookup?id='+ trackId);
   
         xhr.onload = function(){
           if(this.status == 200){
-            let shtml = '';
-            let parsed = JSON.parse(this.responseText);
-            for(var i=0; i < parsed.resultCount ; i++){
-            shtml += '<div class="SongItem" onclick="songDetails('+parsed.results[i].trackId +')"><div class="LeftItem"><h2>' + parsed.results[i].trackName + 
-            '<h2><div class="AlbumArtist"><p>' + parsed.results[i].collectionName+'</p><p> &#8226; </p> <p>' + parsed.results[i].artistName+ 
-            '</p> </div></div><div class="RightItem"> <p>' + millisToMinutesAndSeconds(parsed.results[i].trackTimeMillis)+'</p><p><i class="far fa-heart"></i></p></div></div>';
-            console.log("parsed", parsed.results[i]);
-            }
-            results.innerHTML = shtml;
+            var dhtml = '';
+            var par = JSON.parse(this.responseText);
+            dhtml += '<img src='+par.results[0].artworkUrl100+' alt="" /><div><div class="SongTop"><strong>'+ par.results[0].trackName + '</strong></div><div class="AlbumArtistDetails"><p>'
+            +par.results[0].collectionName + '</p><p>'+millisToMinutesAndSeconds(par.results[0].trackTimeMillis) +'</p><p>' +par.results[0].artistName + '</p></div><p>&#36;'
+            +par.results[0].trackPrice +'</p></div>';
+            console.log("par", par.results[0]);
+            
+            songcontent.innerHTML = dhtml;
           }
         }
   
         xhr.send(); 
   }
 
-  var modal = document.getElementById('songModal');
   
   function songDetails(trackId){
       modal.style.display = "block";
@@ -51,16 +51,16 @@ let textInput = document.querySelector('#textInput');
 
     // Fetch with XHR
     function getItunesAjax(){
-      let text = textInput.value;
+      var text = textInput.value;
       console.log(text);
       if(text != ''){
-        let xhr = new XMLHttpRequest();
+        var xhr = new XMLHttpRequest();
         xhr.open('GET', 'https://itunes.apple.com/search?term='+text+'&media=music&entity=song');
   
         xhr.onload = function(){
           if(this.status == 200){
-            let shtml = '';
-            let parsed = JSON.parse(this.responseText);
+            var shtml = '';
+            var parsed = JSON.parse(this.responseText);
             for(var i=0; i < parsed.resultCount ; i++){
             shtml += '<div class="SongItem" onclick="songDetails('+parsed.results[i].trackId +')"><div class="LeftItem"><h2>' + parsed.results[i].trackName + 
             '<h2><div class="AlbumArtist"><p>' + parsed.results[i].collectionName+'</p><p> &#8226; </p> <p>' + parsed.results[i].artistName+ 
@@ -77,7 +77,7 @@ let textInput = document.querySelector('#textInput');
 
     // Fetch with Fetch API
     function getFactFetch(){
-      let text = textInput.value;
+      var text = textInput.value;
       
       if(text != ''){
         fetch('https://itunes.apple.com/search?term='+text+'&media=music&entity=song')
